25725c17471f683a7d50168706028b39e846d818
Tonia <55912277+Tonia-P@users.noreply.github.com>
2025-05-09 14:54:30 +0300

    merged menu scripts

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -7,19 +7,96 @@ extends PanelContainer
 
 @onready var v_box_container: VBoxContainer = $"MarginContainer2/VBoxContainer"
 
+@onready var startbtn: Button = $"MarginContainer2/VBoxContainer/TextureRect/menu/start"
+@onready var continueBtn: Button = $"MarginContainer2/VBoxContainer/TextureRect/menu/continue"
+@onready var options: Button = $"MarginContainer2/VBoxContainer/TextureRect/menu/options"
+@onready var extras: Button = $"MarginContainer2/VBoxContainer/TextureRect/menu/extras"
+@onready var quitBtn: Button = $"MarginContainer2/VBoxContainer/TextureRect/menu/quit"
+
+# TODO This won't stay for too long.
+@onready var options_menu: VBoxContainer = $"../Settings/VBoxContainer"
+@onready var text_player: AnimationPlayer = $"../Settings/VBoxContainer/Panel/HBoxContainer/Panel/Panel/AnimationPlayer"
+@onready var db: Panel = $"../Settings/VBoxContainer/Panel/HBoxContainer/Panel/Panel"
+@onready var text_dis: RichTextLabel = $"../Settings/VBoxContainer/Panel/HBoxContainer/Panel/Panel/RichTextLabel"
+@onready var settings: Control = $"../Settings"
+
+var content_dict: Dictionary
+var current_page: String
+
+var focusedButton: int = 0
+
 var user_prefs: UserPrefs
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	current_page = "null"
 	user_prefs = UserPrefs.load_or_create()
 	start_label.visible = true
 	menu.visible = false
 	start_animation.play("fade_start")
 	pass # Replace with function body.
+	
+
+# HACK found the children loop hater.
+func _process(_delta: float) -> void:
+	if not startbtn.has_focus() and focusedButton == 0:
+		if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_DOWN) and focusedButton == 0:
+			startbtn.grab_focus()
+			focusedButton = 1
+	if startbtn.is_hovered() and not startbtn.has_focus():
+		focusedButton = 0
+		if continueBtn.has_focus():
+			continueBtn.release_focus()
+		if options.has_focus():
+			options.release_focus()
+		if extras.has_focus():
+			extras.release_focus()
+		if quitBtn.has_focus():
+			quitBtn.release_focus()
+	elif continueBtn.is_hovered() and not continueBtn.has_focus():
+		focusedButton = 0
+		if startbtn.has_focus():
+			startbtn.release_focus()
+		if options.has_focus():
+			options.release_focus()
+		if extras.has_focus():
+			extras.release_focus()
+		if quitBtn.has_focus():
+			quitBtn.release_focus()
+	elif options.is_hovered() and not options.has_focus():
+		focusedButton = 0
+		if startbtn.has_focus():
+			startbtn.release_focus()
+		if continueBtn.has_focus():
+			continueBtn.release_focus()
+		if extras.has_focus():
+			extras.release_focus()
+		if quitBtn.has_focus():
+			quitBtn.release_focus()
+	elif extras.is_hovered() and not extras.has_focus():
+		focusedButton = 0
+		if startbtn.has_focus():
+			startbtn.release_focus()
+		if continueBtn.has_focus():
+			continueBtn.release_focus()
+		if options.has_focus():
+			options.release_focus()
+		if quitBtn.has_focus():
+			quitBtn.release_focus()
+	elif quitBtn.is_hovered() and not quitBtn.has_focus():
+		focusedButton = 0
+		if startbtn.has_focus():
+			startbtn.release_focus()
+		if continueBtn.has_focus():
+			continueBtn.release_focus()
+		if options.has_focus():
+			options.release_focus()
+		if extras.has_focus():
+			extras.release_focus()
+	
+
 
 func _input(event: InputEvent) -> void:
-	print("Kappa keepo")
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and GlobVar.on_start_screen == 0:
 			start_label.visible = false
@@ -54,8 +131,15 @@ func _on_start_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
+	GlobVar.on_start_screen = 2
+	settings.visible = true
+	options_menu.visible = true
+	text_player.play(user_prefs.text_animation)
+	db.self_modulate.a = user_prefs.dialog_box_opacity
+	text_dis.add_theme_constant_override("outline_size", user_prefs.text_outline_size)
 	pass # Replace with function body.
 
 
 func _on_quit_pressed() -> void:
+	get_tree().quit()
 	pass # Replace with function body.
